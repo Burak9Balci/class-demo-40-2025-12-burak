93ceabd0f3589f980cb6253585b7b903fb432835
burak balcı <burak.balci@windowslive.com>
2025-12-30 22:58:04 +0300

    #1 kodlar yazıldı

---
+++ class-demo-40-2025-12-burak/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more informationint[] ogrenciYaslari = new int[100];
+
+string[] ogrenciIsimleri = new string[100];
+
+byte[] sinavNotlari = new byte[100];
+
+sbyte[] sicilDegisimleri = new sbyte[100];
+
+decimal[] urunFiyatlari = new decimal[100];
+
+float[] sicaklikOlcumleri = new float[100];
+
+double[] mesafeOlcumleri = new double[100];
+
+long[] tcKimlikNumaralari = new long[100];
+
+ulong[] dosyaBoyutlari = new ulong[100];
+
+uint[] stokMiktarlari = new uint[100];
+
+short[] dersKodlari = new short[100];
+
+ushort[] plakaKodlari = new ushort[100];
+
